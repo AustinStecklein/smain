3685d16135d5db71bbd8d03fed41dacaa8aaf211
austin <astecklein@gmail.com>
2025-03-17 22:33:49 -0700

    Original two_sum_n2 cleanup

--- basic.s
+++ basic.s
@@ -26,57 +26,58 @@ two_sum_n2:
     # move parameters to gprs or the stack
     push rbp
     mov rbp, rsp
-    sub rsp, 0x48
-    mov qword ptr [rsp+0x10], rdi # int * nums
-    mov dword ptr [rsp+0x18], esi # int numsSize
-    mov dword ptr [rsp+0x20], edx # int target
+    sub rsp, 0x40
+    mov qword ptr [rsp], rdi # int * nums
+    mov dword ptr [rsp+0x08], esi # int numsSize
+    mov dword ptr [rsp+0x10], edx # int target
     mov dword ptr [rcx], 0
-    mov qword ptr [rsp+0x30], rcx # int target
+    mov qword ptr [rsp+0x20], rcx # int target
 
 
     # get some memory the return array
     mov rdi, 8
     call malloc
-    mov qword ptr [rsp+0x40], rax # don't check if malloc failed cause I don't know how to do that
+    mov qword ptr [rsp+0x30], rax # don't check if malloc failed cause I don't know how to do that
 
-    mov rcx, qword ptr [rsp + 0x30]
+    mov rcx, qword ptr [rsp + 0x20]
     # double loop through nums until a combo equals the target
     xor r8, r8  # i
     sub r8, 1
     xor r9, r9  # j
 
 .inner:
-    add r8, 1
+    inc r8
     mov r9, r8
-    add r9, 1 # always start as 1 more than i
-    cmp r8d, dword ptr [rsp+0x18] # first check if we are over size
+    # always start as 1 more than i
+    inc r9
+    cmp r8d, dword ptr [rsp+0x08] # first check if we are over size
     je .cleanup
 
 .outter:
-    cmp r9d, dword ptr [rsp+0x18] # first check if we are over size
+    cmp r9d, dword ptr [rsp+0x08] # first check if we are over size
     je .inner
 
     # perform operation
     xor r10, r10
-    mov r10, [rsp+0x10]
+    mov r10, [rsp]
     mov r10d, dword ptr [r10+r8*4]
-    mov r11, [rsp+0x10]
+    mov r11, [rsp]
     mov r11d, dword ptr [r11+r9*4]
     add r10d, r11d
-    cmp r10d, dword ptr [rsp+0x20]
+    cmp r10d, dword ptr [rsp+0x10]
     je .target
-    add r9, 1
+    inc r9
     jmp .outter
 
 .target:
     # this means the current r8 & r9 are valid
-    lea rax, qword ptr [rsp+0x40]
+    lea rax, qword ptr [rsp+0x30]
     mov dword ptr [rcx], 2
     mov dword ptr [rax], r8d
     mov dword ptr [rax+4], r9d
 
     # cleanup and return
 .cleanup:
-    add rsp, 0x48
+    add rsp, 0x40
     pop rbp
     ret
